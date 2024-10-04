006b404a0c58c22f8080cee971731bee002e7253
romxxxx <romuald.brune@gmail.com>
2024-10-04 15:19:09 +0200

    v1.0.4

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 const (
 	appMajor uint = 1
 	appMinor uint = 0
-	appPatch uint = 3
+	appPatch uint = 4
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
